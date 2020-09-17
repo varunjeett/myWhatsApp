8c4b7e2a795a947aa74723ae6d242897ff389607
varunjeett <abhijeetvarun@gmail.com>
2020-09-17 13:20:33 +0530

    not working

--- backend/dbMessages.js
+++ backend/dbMessages.js
@@ -7,4 +7,4 @@ const whatsappSchema=mongoose.Schema({
     received:Boolean
 });
 
-export default mongoose.model('messagecontent',whatsappSchema);c
+module.exports=mongoose.model('messagecontent',whatsappSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 // import express from 'express';
+
+// import express from `express`;
+
+// import mongoose from `mongoose`;
+
 const express=require('express');
 
 const mongoose=require('mongoose');
+
+
 // import mongoose from 'mongoose';
 
-// const Messages=require('./dbMessages.js');
+const Messages=require('./dbMessages.js');
 
+// import Messages from './dbMessages.js';
 
 //app config
 const app=express();
@@ -14,30 +22,30 @@ const port=(process.env.PORT || 9000);
 
 //middleware
 
-//DB config
-// const connection_url='mongodb+srv://admin:3ILn2uZfTKzQQLCb@cluster0.mchrl.mongodb.net/whatsappdb?retryWrites=true&w=majority';
-// mongoose.connect(connection_url,{
-//     useCreateIndex:true,
-//     useNewUrlParser:true,
-//     useUnifiedTopology:true
-// });
+// DB config
+const connection_url='mongodb+srv://admin:3ILn2uZfTKzQQLCb@cluster0.mchrl.mongodb.net/whatsappdb?retryWrites=true&w=majority';
+mongoose.connect(connection_url,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
 
 //api route
-app.get("/",(req,res) => res.status(200).send('hello world'));
+app.get("/",(req,res) => res.status(200).send('hello leo'));
 
-// app.post('/messages/new',(req,res) => {
-//     const dbMessage=req.body;
+app.post('/messages/new',(req,res) => {
+    const dbMessage=req.body;
 
-//     dbMessages.create(dbMessage,(err,data) =>{
-//         if(err) {
-//             // internal server error
-//             res.status(500).send(err);
-//         } else {
-//             // everything good
-//             res.status(201).send(data);
-//         }
-//     });
-// });
+    dbMessages.create(dbMessage,(err,data) =>{
+        if(err) {
+            // internal server error
+            res.status(500).send(err);
+        } else {
+            // everything good
+            res.status(201).send(data);
+        }
+    });
+});
 
 
 //listen
